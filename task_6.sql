4be67fbe89a174204e21e05f0fa9b16d77a3e0ee
Tumo Matsena <matsenatumo20@gmail.com>
2025-07-05 01:12:02 +0200

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -3,7 +3,11 @@
 -- Insert a row into the customer table
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES 
-    (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave.");
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.');
+
+-- Check if the customer was inserted successfully
+SELECT * FROM customer WHERE customer_id = 2;
+
 
 
 
