10a609d63ad5ed71a3b8775c6394f0f2f6b1e2dc
Tumo Matsena <matsenatumo20@gmail.com>
2025-07-05 00:54:14 +0200

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,6 @@
 -- task_5.sql
 
 -- Insert a single row into the Customers table
-INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO Customers (customer_name, email, address)
+VALUES ('Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 
