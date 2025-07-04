316cc6e8fea0e1df807de4fb7a2fddee4f098f3f
Tumo Matsena <matsenatumo20@gmail.com>
2025-07-05 01:13:54 +0200

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,12 +1,12 @@
 -- task_6.sql
 
--- Insert a row into the customer table
+-- Insert multiple rows into the customer table
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.');
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
 
--- Check if the customer was inserted successfully
-SELECT * FROM customer WHERE customer_id = 2;
 
 
 
