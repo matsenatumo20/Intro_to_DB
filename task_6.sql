696d97504d890c3229c97103f2132e0688e311e7
Tumo Matsena <matsenatumo20@gmail.com>
2025-07-05 01:10:24 +0200

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,12 +1,9 @@
-INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.')
-ON DUPLICATE KEY UPDATE
-    customer_name = VALUES(customer_name),
-    email = VALUES(email),
-    address = VALUES(address);
+-- task_6.sql
+
+-- Insert a row into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES 
+    (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave.");
 
 
 
